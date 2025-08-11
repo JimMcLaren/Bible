4e5da862444d3d2ba2d46670a3d24d16c0f7e939
Jim McLaren <jim@mclaren.us>
2025-08-11 00:06:38 -0400

    Add Bible chapters

---
+++ World English Bible/Source Text/Chapter Bodies/RemoveLeadingZeros.sh
@@ -0,0 +1,237 @@
+current=1CH0
+book=1CH
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1CO0
+book=1CO
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1ES0
+book=1ES
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1JN0
+book=1JN
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1KI0
+book=1KI
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1MA0
+book=1MA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1PE0
+book=1PE
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2CH0
+book=2CH
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2CO0
+book=2CO
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2ES0
+book=2ES
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2JN0
+book=2JN
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2KI0
+book=2KI
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2MA0
+book=2MA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1SA0
+book=1SA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2PE0
+book=2PE
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2SA0
+book=2SA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1TH0
+book=1TH
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2TH0
+book=2TH
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=1TI0
+book=1TI
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=2TI0
+book=2TI
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=ACT0
+book=ACT
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=AMO0
+book=AMO
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=BAR0
+book=BAR
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=COL0
+book=COL
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+# current=DAG;
+# # for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=DAN0
+book=DAN
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=DEU0
+book=DEU
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=ECC0
+book=ECC
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=EPH0
+book=EPH
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+# current=ESG;
+# # for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=EST0
+book=EST
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=EXO0
+book=EXO
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=EZK0
+book=EZK
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=EZR0
+book=EZR
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=FRT0
+book=FRT
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=GAL0
+book=GAL
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=GEN0
+book=GEN
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=GLO0
+book=GLO
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=HAB0
+book=HAB
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=HAG0
+book=HAG
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=HEB0
+book=HEB
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=HOS0
+book=HOS
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=ISA0
+book=ISA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JAS0
+book=JAS
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JDG0
+book=JDG
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JDT0
+book=JDT
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JER0
+book=JER
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JHN0
+book=JHN
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JOB0
+book=JOB
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JOL0
+book=JOL
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JON0
+book=JON
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JOS0
+book=JOS
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=JUD0
+book=JUD
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=LAM0
+book=LAM
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=LEV0
+book=LEV
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=LUK0
+book=LUK
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=MAL0
+book=MAL
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=MAN0
+book=MAN
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=MAT0
+book=MAT
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=MIC0
+book=MIC
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=MRK0
+book=MRK
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=NAM0
+book=NAM
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=NEH0
+book=NEH
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=NUM0
+book=NUM
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=OBA0
+book=OBA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=PHM0
+book=PHM
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=PHP0
+book=PHP
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=PRO0
+book=PRO
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+# current=PS2;
+# # for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=PSA0
+book=PSA
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=REV0
+book=REV
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=ROM0
+book=ROM
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=RUT0
+book=RUT
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=SIR0
+book=SIR
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=SNG0
+book=SNG
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=TIT0
+book=TIT
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=TOB0
+book=TOB
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=WIS0
+book=WIS
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=ZEC0
+book=ZEC
+for f in $current*; do mv "$f" "${f/$current/$book}"; done;
+current=ZEP0
+book=ZEP
+for f in $current*; do mv "$f" "${f/$current/$book}"; done; 
